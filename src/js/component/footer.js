f20a1e942ccde0b450fcb611cc9dca6d86a86fb8
Itsdjkc <118223049+Doctorine06@users.noreply.github.com>
2024-03-23 17:38:01 +0000

    move h1

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -2,9 +2,15 @@ import React, { Component } from "react";
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
+		<h3>Style Op:
+			Opinion Tag _-__
+			
+		</h3>
 		<p>
 			Made with <i className="fa fa-heart text-danger" /> by{" "}
 			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
 		</p>
+			<h1> Case Id Star Wars Clone</h1>
+		
 	</footer>
 );
